5dfaaf3518b0d40bc01c6a443340632a7b717e9f
Yancey Arrington <98492882+YanceyA@users.noreply.github.com>
2025-11-01 16:44:42 +1300

    stm32: map STM32F411 macro to xE variant (#11)
    Merge to test branch.

--- lib/stm32f4/include/stm32f4xx.h
+++ lib/stm32f4/include/stm32f4xx.h
@@ -8,8 +8,8 @@
   *          is using in the C source code, usually in main.c. This file contains:
   *           - Configuration section that allows to select:
   *              - The STM32F4xx device used in the target application
-  *              - To use or not the peripheral’s drivers in application code(i.e. 
-  *                code will be based on direct access to peripheral’s registers 
+  *              - To use or not the peripheralâ€™s drivers in application code(i.e. 
+  *                code will be based on direct access to peripheralâ€™s registers 
   *                rather than drivers API), this option is controlled by 
   *                "#define USE_HAL_DRIVER"
   *  
@@ -68,6 +68,11 @@
 #if !defined  (STM32F4)
 #define STM32F4
 #endif /* STM32F4 */
+
+/* Map legacy STM32F411 define to the expected STM32F411xE variant */
+#if defined(STM32F411) && !defined(STM32F411xE)
+#define STM32F411xE
+#endif
 
 /* Uncomment the line below according to the target STM32 device used in your
    application 
